88e87278a0d9230254537760c303cf7c42fdf31c
shashankgurudu <shashank233@gmail.com>
2016-04-05 00:37:51 +0530

    materialise css

--- results.php
+++ results.php
@@ -1,10 +1,14 @@
-<html>
-<?php
-session_start(); // Starting Session
-{
-$username=$_POST['username'];
-?>
-<head>
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Materialize Navbar with Search bar</title>
+    
+    
+    
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css'>
+
+        <link rel="stylesheet" href="help/css/style2.css">
     <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
@@ -25,9 +29,10 @@ $username=$_POST['username'];
 
         // Create the data table.
         var data = new google.visualization.DataTable();
-        var searchterm = <?php $username ?>;
 
-        var data_file = "http://52.36.166.210:9200/ + 'searchterm' + /_search?size=1000&q=*:*";
+        var searchterm = "<?php $username ?>";
+
+        var data_file = "http://52.36.166.210:9200/"+ searchterm + "/_search?size=1000&q=*:*";
             var http_request = new XMLHttpRequest();
             try{
                // Opera 8.0+, Firefox, Chrome, Safari
@@ -79,7 +84,7 @@ $username=$_POST['username'];
                   ]);
 
         // Set chart options
-                  var options = {'title':'How Much Pizza I Ate Last Night',
+                  var options = {'title':'Real Test Analysis',
                                  'width':400,
                                  'height':300};
 
@@ -99,12 +104,41 @@ $username=$_POST['username'];
 
               }
               //setInterval(drawChart, 2000);
-    </script>
+    </script> 
+
   </head>
-<body>
 
+  <body>
+  <div class="headfirst">
+    <h1 class="white-text main-title">Real Time Text Analysis</h1>
+    
+  </div>
+  
+  
 
- <!--Div that will hold the pie chart-->
-    <div id="chart_div"></div>
-</body>
+    
+<div class="container">
+  <nav>
+    <div class="nav-wrapper"><a href="#" data-activates="nav-mobile" class="right button-collapse"><i class="mdi-navigation-more-vert"></i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+      </ul>
+      <form class="red lighten-3">
+        <div class="input-field">
+          <input id="search" type="search"/>
+          <label for="search"><i class="mdi-action-search"></i></label><i class="mdi-navigation-close close"></i>
+        </div>
+      </form>
+    </div>
+  </nav>
+</div>
+<div id="chart_div"></div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js'></script>
+
+        <script src="help/js/index.js"></script>
+
+    
+    
+    
+  </body>
 </html>
